a2d0d85e77393b937497d41de8ca56ad76ea479c
Rohit42 <rohit.mittapalli@gmail.com>
2018-10-20 17:12:41 -0400

    Updated CORS policy

--- app.js
+++ app.js
@@ -30,6 +30,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Making cors available
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+  next();
+});
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -41,6 +50,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/sections',sectionRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
